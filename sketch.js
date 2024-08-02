d4c09880f391c04ba3d5dab2d9e5d1211d7c289a
FP-OJ <0000112441390xSP@al.educacao.sp.gov.br>
2024-08-02 20:43:10 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(600, 600);
+  }background("pink")
+  
+  
+  function draw() {
+  
+    stroke("pink")
+    fill("black")
+  
+  //console.log(mouseIspressed)
+  
+  if(mouseIsPressed) 
+  rect(mouseX,mouseY,20,35);
+  }
+  
+  
+  
